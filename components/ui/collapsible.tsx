a97965af59a3b4adace7e4fcaf50b38fc1714087
theinfosecguru <60395605+theinfosecguru@users.noreply.github.com>
2025-04-06 23:30:52 +0300

    Add files via upload

---
+++ components/ui/collapsible.tsx
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2025 OryxForge Labs LLC
+ * CyberFusion 4.0 - "Securing Convergence, Empowering Innovation"
+ * All rights reserved.
+ */
+
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+const Collapsible = CollapsiblePrimitive.Root
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
